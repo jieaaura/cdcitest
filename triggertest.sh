17244ae2de84e3dac5fdc72e241516264d74ed9e
Jie Qian <jqian@Auras-MacBook-Pro.local>
2019-08-09 10:36:14 -0700

    add install helm

--- triggertest.sh
+++ triggertest.sh
@@ -1,4 +1,7 @@
 #! /bin/bash -xe
 
-# test
-echo from repo 8/7/2019
+curl -o helm-v2.12.3-darwin-amd64.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.12.3-darwin-amd64.tar.gz
+tar xvf ./helm-v2.12.3-darwin-amd64.tar.gz
+mv darwin-amd64/ /usr/local/bin/
+alias helm-eks="/usr/local/bin/darwin-amd64/helm"
+alias helm="/usr/local/bin/darwin-amd64/helm"
